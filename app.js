8bf29506de19a167d70e785db31703524ed33db6
jzhupan <jzhupan@gmail.com>
2023-04-07 21:26:28 -0400

    Updated the weather app to display location and country.

--- app.js
+++ app.js
@@ -7,20 +7,25 @@ const weatherTemperature = document.getElementById('temp');
 const weatherCondition = document.getElementById('weatherText');
 
 button.addEventListener('click', async () => {
-    let cityName = input.value;
+    let cityNameInput = input.value;
     let response = await axios.get(
-        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=1&aqi=no&alerts=no`
+        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityNameInput}&days=1&aqi=no&alerts=no`
 )
-    let todaysWeather = response.data.current.temp_f
+    let cityName = response.data.location.name
+    let countryName = response.data.location.country
     
+    let todaysWeather = response.data.current.temp_f
     let weatherText = response.data.current.condition.text
     let weatherIcon = response.data.current.condition.icon
     
     weatherCondition.innerHTML = weatherText;
     weatherImg.src = weatherIcon;
 
-    cityNameDisplay.innerHTML = cityName.toUpperCase();
+    let cityAndCountryName = `${cityName} , ${countryName}`;
+    cityNameDisplay.innerHTML = cityAndCountryName;
     weatherTemperature.innerHTML = `${todaysWeather}ºF`
+    
+    console.log(countryName)
     console.log(response)
     console.log(cityName)
     console.log(todaysWeather)
